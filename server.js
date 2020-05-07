80c0efe55304eeb8eb28785a94d30b466efb1508
mingjing tang <tmjcsh@gmail.com>
2020-05-07 19:46:20 -0400

    test to delete env PORT

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
+app.use(favicon(__dirname + "/build/favicon.ico"));
+
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/ping", function (req, res) {
